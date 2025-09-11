07b882a06a8e8c48be368c548df44cbc80bde775
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-09-11 12:53:34 -0600

    Enhance user state management in useUsuario hook: Refactor to utilize useEffect for setting user state upon loading, improving data handling and ensuring user information is accurately reflected.

--- src/hooks/Usuarios/Usuario.ts
+++ src/hooks/Usuarios/Usuario.ts
@@ -1,9 +1,16 @@
 import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import type { UserResource } from "@clerk/types";
 
 export function useUsuario() {
-    const { user } = useUser();
+    const { user: usuario, isLoaded } = useUser();
+    const [user, setUser] = useState<UserResource | null>(null);
+
+    useEffect(() => {
+        if (isLoaded && usuario) {
+            setUser(usuario);
+        }
+    }, [isLoaded, usuario]);
 
     return { user };
-
 }
-
